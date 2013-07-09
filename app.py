61565c5eb763c141016c5c3933a7e9057c010b1f
mies <mies@wercker.com>
2013-07-09 23:37:14 +0200

    added application skeleton

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, Response, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  data = []
+  resp = Response(json.dumps(data), status=200, mimetype='application/json')
+  return resp
+
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
