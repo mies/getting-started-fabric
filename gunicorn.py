874b0f6421e167282c5c848a34ad6a1f6b3f4a3f
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-07-10 15:25:39 +0200

    updated gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,9 +1,14 @@
 import os
-bind = 
-workers
-backlog
-worker_class
+
+def number_of_cpus():
+  if not hasattr(os, "sysconf"):
+    raise RuntimeError("No sysconf detected.")
+  return os.sysconf("SC_NPROCESSORS_ONLN")
+bind = "0.0.0.0:5000"
+workers = number_of_cpus() * 2 + 1
+backlog = 2048
+worker_class = "gevent"
 debug = True
 daemon = True
-pidfile = 
-logfile =
+pidfile =  "/tmp/gunicorn.pid"
+logfile = "/tmp/gunicorn.log"
