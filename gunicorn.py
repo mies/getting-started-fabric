2b61be450e19c32984bbab9029985ee1b285ac0b
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-07-10 15:51:55 +0200

    gevent

--- gunicorn.py
+++ gunicorn.py
@@ -1,14 +1,11 @@
-import os
+import multiprocessing
 
-def number_of_cpus():
-  if not hasattr(os, "sysconf"):
-    raise RuntimeError("No sysconf detected.")
-  return os.sysconf("SC_NPROCESSORS_ONLN")
 bind = "0.0.0.0:5000"
-workers = number_of_cpus() * 2 + 1
+workers = multiprocessing.cpu_count() * 2 + 1
 backlog = 2048
 worker_class = "gevent"
 debug = True
 daemon = True
 pidfile =  "/tmp/gunicorn.pid"
 logfile = "/tmp/gunicorn.log"
+proc_name = "flask"
